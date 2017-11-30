775790234e2b8aa58c48fd35a5691a292a6b65fe
YAMADA, Tetsuo <tyam001@gmail.com>
2017-11-30 20:47:46 +0900

    import XMethodHandler

---
+++ lib/XMethodHandler.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * XMethodHandler
+ *
+ * relay/middleware実装。ブラウザから任意のHTTPメソッドを実行できるようにする。
+ * 具体的には、X-METHODヘッダがセットされていればそのメソッドとして扱い、
+ * またリクエストが$_POST['__METHOD']を含む場合はそのメソッドとして扱う。
+ * tyam\radarxに移動のこと。
+ */
+
+namespace Custom;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class XMethodHandler
+{
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $xmethod = $this->detectMethod($request);
+        if ($xmethod !== $request->getMethod()) {
+            $request = $request->withMethod($xmethod);
+        }
+        return $next($request, $response);
+    }
+
+    protected function detectMethod(Request $request)
+    {
+        if ($request->hasHeader('X-METHOD')) {
+            return $request->getHeader('X-METHOD')[0];
+        }
+
+        $post = $request->getParsedBody();
+        if (isset($post['__METHOD'])) {
+            return $post['__METHOD'];
+        }
+
+        return $request->getMethod();
+    }
+}
